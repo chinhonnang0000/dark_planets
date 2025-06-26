5cab4471bad0085fb9149c4db4f4582b67ddbffb
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2025-06-27 07:38:44 +0800

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,9 @@
+require("dapl_main.js"); 
 
+// check script working
+Events.on(ContentInitEvent, e =>
+{ 
+   Vars.content.planet("erekir").allowLaunchToNumbered = true;  
+   Vars.content.planet("erekir").hiddenItems = new Seq();
+   Vars.content.planet("serpulo").hiddenItems = new Seq(); 
+});
