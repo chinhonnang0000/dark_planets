03c21bca60ef714e80cd1ab1ce585d05514eeee3
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2025-06-27 08:10:09 +0800

    Update dapl_main.js

--- scripts/dapl_main.js
+++ scripts/dapl_main.js
@@ -1,1 +1,12 @@
+Events.on(ContentInitEvent, e =>
+  {
+    init_random();
+  })
+var dapl_main_gen = extenc(SerpuloPlanetGenerator,{});
 
+var floor_0_tiles = [Blocks.magmarock,Blocks.hotrock,Blocks.basalt]; // to insert core zone at random. 
+var floor_1_tiles = [Blocks.watar, Blocks.darksandWater, Blocks.darksand];
+var floor_stones = [];
+
+function init_random(){}
+function generate_tile_system(){} 
