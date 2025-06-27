ae153caf7c139360f832d292ef4eb73ed2a67a73
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2025-06-27 09:58:39 +0800

    Update dapl_main.js

--- scripts/dapl_main.js
+++ scripts/dapl_main.js
@@ -6,7 +6,9 @@ Events.on(ContentInitEvent, e =>
   })
 var dapl_main_gen = extenc(SerpuloPlanetGenerator,
 {
-                             
+getColor(po,co){var bl = this.getBlock(po); if(bl == Blocks.salt) bl = Blocks.sand; co.set(block.mapColor).a(1 - bl.albedo);},  
+rawHeight(po){return (Mathf.pow(Simplex.noise3d(seed, 7, 0.5, 0.34, position.x * scl, position.y * scl + heightYOffset, position.z * scl) * heightScl, 2.3) + wos) / (1 + wos);},
+  
 });
 var floors_arky = [Blocks.arkyciteFloor, Blocks.arkyicStone, Blocks.BeryllicStone]
 var floors_volc = [Block.slag,Blocks.magmarock,Blocks.hotrock,Blocks.basalt]; // to insert core zone at random. 
@@ -15,7 +17,13 @@ var floors_wlig = [Blocks.water, Blocks.sandwater, Blocks.sand];
 var floors_spor = [Blocks.sporeMoss, Blocks.moss, Blocks.redStone];
 var floors_cold = [Blocks.ice,Blocks.iceSnow,Blocks.snow]; 
 
-var total_ties =[]; 
+function get_block()
+{
+    return Blocks.coreZone;
+}
 
-function init_random(){}
+var total_ties =[]; 
+var wos = 0.07
+
+function init_random(){wos = Math.random()* 0.15;}
 function generate_tile_system(){} 
