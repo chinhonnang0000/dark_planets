ea82b42d4fa28bf43820b99533a67fdc3abb5d9a
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2025-06-27 10:36:20 +0800

    Update dapl_main.js

--- scripts/dapl_main.js
+++ scripts/dapl_main.js
@@ -3,12 +3,16 @@ Events.on(ContentInitEvent, e =>
     init_random();
     var this_pl = Vars.content.planet("dark_planets-dapl_main")
     this_pl.sectorSeed = Math.floor(Math.random() * 999999999);
-    this_pl.meshLoader = new HexMesh(this_pl,new HexMesher(this_pl),9);
+    //this_pl.meshLoader = new HexMesh(this_pl,new HexMesher(this_pl),9); //shelved hex mesh
     this_pl.generator = new SerpuloPlanetGenerator();
   })
 var dapl_main_gen = extenc(SerpuloPlanetGenerator,
 {
 getColor(po,co){var bl = this.getBlock(po); if(bl == Blocks.salt) bl = Blocks.sand; co.set(block.mapColor).a(1 - bl.albedo);},  
+generate()
+{
+  init_random();  
+}
 rawHeight(po){return (Mathf.pow(Simplex.noise3d(seed, 7, 0.5, 0.34, position.x * scl, position.y * scl + heightYOffset, position.z * scl) * heightScl, 2.3) + wos) / (1 + wos);},
   
 });
@@ -27,5 +31,5 @@ function get_block()
 var total_ties =[]; 
 var wos = 0.07
 
-function init_random(){wos = Math.random()* 0.15;}
+function init_random(){wos = Math.random()* 0.15;  this_pl.sectorSeed = Math.floor(Math.random() * 999999999);}
 function generate_tile_system(){} 
