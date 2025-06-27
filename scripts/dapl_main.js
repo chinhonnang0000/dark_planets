fc04720d75a7c32f068dc7f4652333c2c8960e0b
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2025-06-27 09:28:41 +0800

    Update dapl_main.js

--- scripts/dapl_main.js
+++ scripts/dapl_main.js
@@ -3,11 +3,18 @@ Events.on(ContentInitEvent, e =>
     init_random();
     Vars.content.planet("dark_planets-dapl_main").generator = new SerpuloPlanetGenerator();
   })
-var dapl_main_gen = extenc(SerpuloPlanetGenerator,{});
+var dapl_main_gen = extenc(SerpuloPlanetGenerator,
+{
+                             
+});
+var floors_arky = [Blocks.arkyciteFloor, Blocks.arkyicStone, Blocks.BeryllicStone]
+var floors_volc = [Block.slag,Blocks.magmarock,Blocks.hotrock,Blocks.basalt]; // to insert core zone at random. 
+var floors_wdar = [Blocks.watar, Blocks.darksandWater, Blocks.darksand];
+var floors_wlig = [Blocks.water, Blocks.sandwater, Blocks.sand];
+var floors_spor = [Blocks.sporeMoss, Blocks.moss, Blocks.redStone];
+var floors_cold = [Blocks.ice,Blocks.iceSnow,Blocks.snow]; 
 
-var floor_0_tiles = [Blocks.magmarock,Blocks.hotrock,Blocks.basalt]; // to insert core zone at random. 
-var floor_1_tiles = [Blocks.watar, Blocks.darksandWater, Blocks.darksand];
-var floor_stones = [];
+var total_ties =[]; 
 
 function init_random(){}
 function generate_tile_system(){} 
