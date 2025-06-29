16c9fd7ff2822f222e58f00a5c5a0d23aaab232b
chinhonnang0000 <106854561+chinhonnang0000@users.noreply.github.com>
2025-06-29 18:33:18 +0800

    Update dapl_main.js

--- scripts/dapl_main.js
+++ scripts/dapl_main.js
@@ -179,9 +179,9 @@ genTile(po,ti)
    else if(ti.floor == Blocks.yellowStone && Simplex.noise3d(seed_vyel,1,0,dist_vyel,po.x,po.y,po.z) > 0.75){ti.floor = Blocks.yellowStoneVent; ti.block = Blocks.air;}
 
    // adding resources here does not work. Adding props
-  if(Simplex.noise3d(seed_coal,1,0,dist_coal,po.x,po.y,po.z) > 0.7){ti.setOverlay(Blocks.oreCoal);}
-  if(Simplex.noise3d(seed_copp,1,0,dist_copp,po.x,po.y,po.z) > 0.7){ti.setOverlay(Blocks.oreCopper);}
-  if(Simplex.noise3d(seed_lead,1,0,dist_lead,po.x,po.y,po.z) > 0.7){ti.setOverlay(Blocks.oreLead);}
+  if(Simplex.noise3d(seed_coal,1,0,dist_coal,po.x,po.y,po.z) > 0.7){ti.overlay.asFloor()=Blocks.oreCoal;}
+  if(Simplex.noise3d(seed_copp,1,0,dist_copp,po.x,po.y,po.z) > 0.7){ti.overlay.asFloor()=Blocks.oreCopper;}
+  if(Simplex.noise3d(seed_lead,1,0,dist_lead,po.x,po.y,po.z) > 0.7){ti.overlay.asFloor()=Blocks.oreLead;}
 },
 noiseOct(x, y, octaves, falloff, scl){
     var v = this.sector.rect.project(x, y).scl(5);
